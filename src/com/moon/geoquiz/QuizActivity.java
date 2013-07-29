8143c13a7bfe202ea6e7d971c032477d73ccca38
Moonbloom <kasperliholm@hotmail.com>
2013-07-29 20:23:03 +0200

    update

--- src/com/moon/geoquiz/QuizActivity.java
+++ src/com/moon/geoquiz/QuizActivity.java
@@ -2,19 +2,21 @@ package com.moon.geoquiz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends Activity {
 
+	private static final String TAG = "QuizActivity";
+	private static final String KEY_INDEX = "index";
+	
 	private Button mTrueButton;
 	private Button mFalseButton;
-	private ImageButton mNextButton;
-	private ImageButton mPreviousButton;
+	private Button mNextButton;
 	private TextView mQuestionTextView;
 	
 	private TrueFalse[] mQuestionBank = new TrueFalse[] {
@@ -54,8 +56,9 @@ public class QuizActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Log.d(TAG,"onCreate(Bundle) called");
 		setContentView(R.layout.activity_quiz);
-
+		
 		mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
 		mQuestionTextView.setOnClickListener(new View.OnClickListener() {			
 			@Override
@@ -83,7 +86,7 @@ public class QuizActivity extends Activity {
 			}
 		});
 			
-		mNextButton = (ImageButton)findViewById(R.id.next_button_id);
+		mNextButton = (Button)findViewById(R.id.next_button_id);
 		mNextButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) 
@@ -93,21 +96,20 @@ public class QuizActivity extends Activity {
 			}
 		});
 		
-		mPreviousButton = (ImageButton)findViewById(R.id.previous_button_id);
-		mPreviousButton.setOnClickListener(new View.OnClickListener() {			
-			@Override
-			public void onClick(View v) {
-				if (mCurrentIndex > 0)
-				{
-					mCurrentIndex = (mCurrentIndex-1);
-					updateQuestion();
-				}
-			}
-		});
+		if (savedInstanceState != null) {
+			mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+		}
 		
 		// This makes a question appear at first when opening the app
 		updateQuestion();
 		
+	}
+	
+	@Override
+	public void onSaveInstanceState(Bundle savedInstanceState) {
+		super.onSaveInstanceState(savedInstanceState);
+		Log.i(TAG,"onSaveInstanceState() called");
+		savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
 	}
 
 	@Override
@@ -115,6 +117,36 @@ public class QuizActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.quiz, menu);
 		return true;
+	}
+	
+	@Override
+	public void onStart() {
+		super.onStart();
+		Log.d(TAG,"onStart() called");
+	}
+	
+	@Override
+	public void onPause() {
+		super.onPause();
+		Log.d(TAG,"onPause() called");
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		Log.d(TAG,"onResume() called");
+	}
+	
+	@Override
+	public void onStop() {
+		super.onStop();
+		Log.d(TAG,"onStop() called");
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		Log.d(TAG,"onDestroy() called");
 	}
 
 }
